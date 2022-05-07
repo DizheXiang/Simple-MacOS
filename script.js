bc3e29d2af0d42eec8fe62ace20cf97ac107ac87
Dizhe Xiang <dizhexiang@gmail.com>
2022-05-06 22:44:43 -0700

    add new icons

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 
-
 updateTime();
 
-function updateTime(){
+function updateTime() {
     var str = "";
     var now = new Date();
     
     var hours = now.getHours();
+    if(hours > 11){
+        hours -= 12;
+    }
     var days = new Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
     var months = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
     str += days[now.getDay()] +  " " + months[now.getMonth()] + " " + now.getDate() 
-    + " "+ now.getHours() +":" + now.getMinutes();
-    if(hours > 11){
+    + " "+ hours +":" + now.getMinutes();
+    if(now.getHours() > 11){
         str += " PM";
     } else {
         str += " AM";
